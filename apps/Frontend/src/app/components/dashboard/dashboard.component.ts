32445ebb7c5d8d1dc33da65e9326aed53581e8c1
ecanel-2022406 <ecanel-2022406@kinal.edu.gt>
2026-08-19 10:13:09 -0600

    refactor: se agregó el vencimiento del token para cerrar sesión, ecanel-2022406

--- apps/Frontend/src/app/components/dashboard/dashboard.component.ts
+++ apps/Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
+  imports: [CommonModule],
+  template: `
+    <div style="padding: 40px; text-align: center;">
+      <h2>Panel de Control (Dashboard)</h2>
+      <p>Has iniciado sesión correctamente.</p>
+      <p style="color: red; font-weight: bold;">Tu sesión se cerrará automáticamente en cuanto el token venza...</p>
+    </div>
+  `,
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private router = inject(Router);
 
+  ngOnInit() {
+    this.validarTokenYExpiracion();
+  }
+
+  validarTokenYExpiracion() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    try {
+      const payloadBase64 = token.split('.')[1];
+      const payloadDecoded = JSON.parse(atob(payloadBase64));
+      
+      const expiracionMs = payloadDecoded.exp * 1000;
+      const tiempoRestante = expiracionMs - Date.now();
+
+      if (tiempoRestante <= 0) {
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+      } else {
+
+        setTimeout(() => {
+          localStorage.removeItem('token');
+          this.router.navigate(['/login']);
+        }, tiempoRestante);
+      }
+    } catch (error) {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    }
+  }
 }
--- apps/backend/src/controller/auth.controller.ts
+++ apps/backend/src/controller/auth.controller.ts
@@ -43,7 +43,7 @@ export const loginUsuario = async (req: Request, res: Response) => {
     const token = jwt.sign(
       { id: usuario.id, email: usuario.email }, 
       JWT_SECRET, 
-      { expiresIn: '10s' } 
+      { expiresIn: '1m' } 
     );
 
     return res.json({ 
